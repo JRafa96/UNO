bf5d8f08c79bab09a465c56b3018c6eaff480daa
João Rafael <47887885+JRafa96@users.noreply.github.com>
2019-06-04 11:38:00 +0100

    Criação da classe cor
    enum cor

--- src/jogo/CartaNumero.java
+++ src/jogo/CartaNumero.java
@@ -3,17 +3,17 @@ package jogo;
 public class CartaNumero extends Carta {
 
 	private int numero;
-	private char cor;
+	private Cor cor;
 
 	public int getNumero() {
 		return numero;
 	}
 
-	public char getCor() {
+	public Cor getCor() {
 		return cor;
 	}
 
-	public CartaNumero(int numero, char cor) {
+	public CartaNumero(int numero, Cor cor) {
 		super();
 		this.numero = numero;
 		this.cor = cor;
---
+++ src/jogo/Cor.java
@@ -0,0 +1,11 @@
+package jogo;
+
+public enum Cor {
+	AMARELO(0), VERMELHO(1), VERDE(2), AZUL(3);
+
+	private int nCor;
+
+	private Cor(int n) {
+		nCor = n;
+	}
+}
