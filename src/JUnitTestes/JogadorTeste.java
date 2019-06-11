71d1654799fd0303db5be67d244fee4ccfe97b08
BernardoMG04 <bernas.mg04@gmail.com>
2019-06-11 11:23:15 +0100

    Criação de JUnits

---
+++ src/JUnitTestes/JogadorTeste.java
@@ -0,0 +1,20 @@
+package JUnitTestes;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class JogadorTeste {
+
+	@BeforeEach
+	void setUp() throws Exception {
+		
+	}
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
