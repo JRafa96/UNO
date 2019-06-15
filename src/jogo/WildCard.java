32730614385928ce7a45879e98616fbd84af2b00
João Rafael <47887885+JRafa96@users.noreply.github.com>
2019-06-15 18:11:11 +0100

    Documentação da classe WildCard

--- src/jogo/WildCard.java
+++ src/jogo/WildCard.java
@@ -2,19 +2,37 @@ package jogo;
 
 import java.util.Scanner;
 
+/**
+ * A classe <code>ActionCard</code> é constituida por um tipo e uma ação que vai
+ * ser determinada de acordo com o seu tipo.
+ * @author João Rafael
+ *
+ */
 public class WildCard extends CartaEspecial {
 
 	private Tipo tipo;
 
+	/**
+	 * Construtor da <code>WildCard</code>, recebe um tipo.
+	 * @param tipo
+	 */
 	public WildCard(Tipo tipo) {
 		super();
 		this.tipo = tipo;
 	}
 
+	/**
+	 * Método de acesso (leitura) ao tipo da carta.
+	 * @return
+	 */
 	public Tipo getTipo() {
 		return tipo;
 	}
 
+	/**
+	 * Método chamado pelo jogo que vai pedir ao utilizador para introduzir uma cor.
+	 * @return cor
+	 */
 	private Cor mudarCor() {
 		String escolha;
 		do {
@@ -27,6 +45,11 @@ public class WildCard extends CartaEspecial {
 	}
 
 
+	/**
+	 * Ação que a carta vai realizar no <code>jogo</code>.
+	 * 
+	 * @throws Mais_4 se a carta for do tipo MAIS_4
+	 */
 	@Override
 	public void açao() throws Exception{
 		mudarCor();
