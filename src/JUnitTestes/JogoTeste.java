27394dc8a9002cdd600342a54566374fcc9335e0
wtficctsn <jesus98.pt@gmail.com>
2019-06-17 16:01:09 +0100

    Update JogoTeste.java

--- src/JUnitTestes/JogoTeste.java
+++ src/JUnitTestes/JogoTeste.java
@@ -3,7 +3,10 @@ package JUnitTestes;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import jogo.*;
 
 class JogoTeste {
 
@@ -12,8 +15,21 @@ class JogoTeste {
 	}
 
 	@Test
+	@DisplayName("╯°□°）╯ ||____|| jogador foi buscar cartas dps de +2 ou +4?")
 	void test() {
+		Baralho b1=new Baralho();
+		Jogo j1=new Jogo();
+		Jogador pl1 = new Jogador("bot sadam");
+		Jogador pl2 = new Jogador("hallauh AKBAR");
+		j1.getJogadores().add(pl1);
+		j1.getJogadores().add(pl2);
+		Carta carta = new WildCard(Tipo.values()[3]);
+		j1.setBaralho(b1);
+		
+		
+		
 		fail("Not yet implemented");
 	}
+	
 
 }
