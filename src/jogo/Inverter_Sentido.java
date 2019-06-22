edc5279a7f60129c1e955c73e9ec0746d38f2475
BernardoMG04 <bernas.mg04@gmail.com>
2019-06-22 15:42:21 +0100

    JavaDoc da exceção Inverter_Sentido

--- src/jogo/Inverter_Sentido.java
+++ src/jogo/Inverter_Sentido.java
@@ -1,5 +1,10 @@
 package jogo;
 
+/**
+ * É uma exceção que é lançada quando um jogador joga uma carta do tipo Inverter_Sentido.
+ * @author Bernardo
+ *
+ */
 public class Inverter_Sentido extends Exception{
 
 	public Inverter_Sentido() {
